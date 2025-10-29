860de85a82672cb837da9788a59bd7326254a6e9
Douglas <104152286+dgsrosa@users.noreply.github.com>
2025-10-29 11:21:43 -0300

    Refactor to use Express for IP reflection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-export default {
-  async fetch(request) {
-    return new Response(request.headers.get("x-forwarded-for"), {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  res.send(ip);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`IP Reflector rodando na porta ${port}`);
+});
